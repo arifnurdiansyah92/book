12efb4ef620a8067a39aa88162a7b534b8ed15be
ANDEV <arifnurdiansyah92@gmail.com>
2020-09-18 15:37:57 +0700

    Implement Middleware

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.auth.login');
-})->name("home");
+})->name("home")->middleware(['guest']);
 
 Route::group([
-    "prefix" => "master-data"
+    "prefix" => "master-data",
+    "middleware" => ["admin"]
 ],function(){
     Route::get('/category','CategoryController@index')->name('category.index');
     Route::get('/category/create','CategoryController@create')->name('category.create');
@@ -40,9 +41,10 @@ Route::group([
     "prefix" => "book"
 ],function(){
     Route::get('/','BookController@index')->name('book.index');
-    Route::get('/create','BookController@create')->name('book.create');
-    Route::post('/','BookController@store')->name('book.store');
-    Route::get('/{id}','BookController@show')->name('book.show');
-    Route::put('/{id}','BookController@update')->name('book.update');
-    Route::get('/{id}/delete','BookController@delete')->name('book.delete');
+    Route::get('/create','BookController@create')->name('book.create')->middleware(['admin']);
+    Route::post('/','BookController@store')->name('book.store')->middleware(['admin']);
+    Route::get('/{id}','BookController@show')->name('book.show')->middleware(['admin']);
+    Route::put('/{id}','BookController@update')->name('book.update')->middleware(['admin']);
+    Route::get('/{id}/delete','BookController@delete')->name('book.delete')->middleware(['admin']);
+    Route::get('/{pretty_url}','BookController@show_by_url')->name('book.detail');
 });
